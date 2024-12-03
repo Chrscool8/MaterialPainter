748632bab02aabb8b112e59a4c724c2da4c7c079
Chrscool8 <ChrisBradel14@gmail.com>
2024-12-03 18:07:00 -0500

    fix for new update

--- Src/MP2.cs
+++ Src/MP2.cs
@@ -54,7 +54,7 @@ namespace MaterialPainter2
 
     public class MP2 : AbstractMod, IModSettings
     {
-        public const string VERSION_NUMBER = "241026";
+        public const string VERSION_NUMBER = "241203";
 
         public override string getIdentifier() => "MaterialPainter";
 
@@ -888,10 +888,10 @@ namespace MaterialPainter2
     public class LoadGetFileNamePatch
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "Sall Good")]
-        private static MethodBase TargetMethod() => AccessTools.Method(typeof(Loader), "loadSavegame", parameters: new Type[] { typeof(string), typeof(GameController.GameMode), typeof(ParkSettings), typeof(bool), typeof(bool), typeof(bool), typeof(OnParkLoadedHandler), typeof(SerializationContext.Context) });
+        private static MethodBase TargetMethod() => AccessTools.Method(typeof(Loader), "loadSavegame", parameters: new Type[] { typeof(string), typeof(GameController.GameMode), typeof(ParkSettings), typeof(bool), typeof(bool), typeof(bool), typeof(OnParkLoadedHandler),typeof(OnParkDeserializedHandler), typeof(SerializationContext.Context) });
 
         [HarmonyPrefix]
-        public static void Prefix(string filePath, GameMode gameMode, ParkSettings settings, bool rememberFilePath, bool newPark, bool showNewParkUI = true, GameController.OnParkLoadedHandler onParkLoadedHandler = null, SerializationContext.Context context = (SerializationContext.Context)0)
+        public static void Prefix(string filePath, GameMode gameMode, ParkSettings settings, bool rememberFilePath, bool newPark, bool showNewParkUI = true, GameController.OnParkLoadedHandler onParkLoadedHandler = null, GameController.OnParkDeserializedHandler onParkDeserializedHandler = null, SerializationContext.Context context = (SerializationContext.Context)0)
         {
             MP2.MPDebug(filePath);
             MP2.current_file_path = filePath;
